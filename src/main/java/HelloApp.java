bd474da27a4b81af4d248d5ffdb69b0d564c5f5d
Pranav bhama <pranavbhama28@gmail.com>
2026-04-06 15:21:30 +0530

    feat: implement UC5 - process names using enhanced for loop

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,8 +1,9 @@
+// UC5: Using enhanced for-loop
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
             for (String name : args) {
-                System.out.println("Hello " + name);
+                System.out.println("Hello " + args[0]); // or name
             }
         } else {
             System.out.println("Hello World");
