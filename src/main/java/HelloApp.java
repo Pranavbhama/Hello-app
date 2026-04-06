f6c5ec6f5a7f83123cc91feb0e07b6b14c81e633
Pranav bhama <pranavbhama28@gmail.com>
2026-04-06 15:31:31 +0530

    feat: implement UC7 - format names using String.join

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,13 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
+        // UC7: Use String.join for efficient formatting
         if (args.length > 0) {
-            String greeting = "Hello ";
-            for (String name : args) {
-                greeting += name + ", ";
-            }
-            // UC6: Use substring to remove the trailing ", " (last 2 characters)
-            greeting = greeting.substring(0, greeting.length() - 2);
-            System.out.println(greeting);
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
         } else {
             System.out.println("Hello World");
         }
