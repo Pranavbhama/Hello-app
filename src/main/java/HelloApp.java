8cebb894ecbee162b68a76ffa475443ec32eb8e8
Pranav bhama <pranavbhama28@gmail.com>
2026-04-06 15:29:19 +0530

    feat: implement UC6 - format multiple names using substring

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,9 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
+            String greeting = "Hello ";
             for (String name : args) {
-                System.out.println("Hello " + name);
+                greeting += name + ", ";
             }
+            // UC6: Use substring to remove the trailing ", " (last 2 characters)
+            greeting = greeting.substring(0, greeting.length() - 2);
+            System.out.println(greeting);
         } else {
             System.out.println("Hello World");
         }
